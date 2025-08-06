52f4c5fb071130cef6bda9ae635a0bd51cddf21a
Yurex <yusufekungomi@gmail.com>
2025-08-06 23:15:51 +0100

    turned the notification into a dialog for smaller screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,19 @@
 'use client';
-import { BellDotIcon, Ellipsis, Menu } from 'lucide-react';
+import { BellDotIcon, Menu } from 'lucide-react';
 import { useState } from 'react';
 import { Logo } from './Logo';
 import Notifications from './Notifications';
 import { UserAvatar } from './UserAvatar';
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogOverlay,
+} from './ui/dialog';
 
 export function Header() {
   const [showNotification, setShowNotification] = useState(false);
+  const handleShowNotification = () => setShowNotification((cur) => !cur);
   return (
     <>
       <header className='fixed top-0 z-10 flex w-full items-center justify-center bg-white shadow-sm'>
@@ -14,12 +21,13 @@ export function Header() {
           <Logo />
           <div className='flex items-center gap-x-4'>
             <BellDotIcon
+              onClick={handleShowNotification}
               size={30}
-              onClick={() => setShowNotification((cur) => !cur)}
               className={`block xl:hidden ${
                 showNotification ? 'text-blue-500' : ''
               }`}
             />
+
             <UserAvatar />
             {/* <span>Yurex</span> */}
             <Menu />
@@ -27,9 +35,12 @@ export function Header() {
         </div>
       </header>
 
-      {showNotification && (
-        <Notifications className='fixed top-20 right-4 w-md z-20 h-[88%] s' />
-      )}
+      <Dialog open={showNotification} onOpenChange={handleShowNotification}>
+        <DialogContent showCloseButton={false} className='w-md h-[88%]'>
+          <DialogOverlay className='bg-transparent/0' />
+          <Notifications />
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
